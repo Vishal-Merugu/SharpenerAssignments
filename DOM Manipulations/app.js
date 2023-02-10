516e568624cecc2e93680baec0e2287e2dd80bb9
Vishal-Merugu <vishalvish360@gmail.com>
2023-02-10 21:55:34 +0530

    renamed directory

---
+++ DOM Manipulations/app.js
@@ -0,0 +1,30 @@
+
+document.addEventListener("DOMContentLoaded", () =>{
+    document.querySelector('form').onsubmit = () => {
+        const item = document.querySelector('input[type = "text"]');
+        if (item.value.length > 0 ){
+            const li = document.createElement('li');
+            li.innerHTML = item.value ;
+            li.classList.add("list-group-item");
+            document.querySelector('#items').append(li);
+            item.value = "";
+        }
+        return false;
+    }
+
+    const header = document.querySelector('#main-header');
+    header.className = "bg-dark text-white p-4 mb-3"
+    // document.querySelector('#main-header').className = "bg-dark text-white p-4 mb-3";
+
+    const additems = document.querySelector(".title")
+    additems.classList.add('font-weight-bold','text-success');
+
+    const items = document.querySelectorAll(".list-group-item")
+    const third = items[2]
+    third.style.backgroundColor = 'green' ;
+
+    for(var i = 0 ; i < items.length ; i++){
+        items[i].style.fontWeight = "bold";
+    }
+});
+
