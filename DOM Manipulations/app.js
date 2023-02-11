e4684b06affd14fc5a32747ce947f25503ffcca7
Vishal-Merugu <vishalvish360@gmail.com>
2023-02-11 15:24:23 +0530

    Assignment - 7

--- DOM Manipulations/app.js
+++ DOM Manipulations/app.js
@@ -30,5 +30,71 @@ document.addEventListener("DOMContentLoaded", () =>{
     fifth = items[4]
     fifth.classList.add("list-group-item");
 
+    items[1].style.backgroundColor = "green";
+    items[2].style.backgroundColor = "white";
+    items[2].classList.add('display:none');
+
 });
+
+
+//traversing DOM
+var itemlist  =  document.querySelector("#items");
+
+//parentNode = parentElement
+// console.log(itemlist.parentNode);
+// itemlist.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemlist.parentNode.parentNode);
+
+//childNodes
+// console.log(itemlist.childNodes);
+//children is better than childNodes bcoz childNodes give line breaks and text also
+
+//children
+// console.log(itemlist.children);
+// itemlist.children[0].style.backgroundColor = "yellow";
+
+// similarily firstelementChild is better then firstChild
+//firstChild and firstElementChild
+// console.log(itemlist.firstChild);
+// console.log(itemlist.firstElementChild);
+
+//again lastElementChild is better than lastChild
+// lastChild and lastElementChild
+// console.log(itemlist.lastChild);
+// console.log(itemlist.lastElementChild);
+
+
+// similarily nextElementSibling is better than nextSibling
+//nextSibling and nextElementSibling
+var title = document.querySelector('.title')
+// console.log(title.nextSibling);
+// console.log(title.nextElementSibling);
+
+//similarily previousElementSibling is better than previousSibling
+// previousSibling and PreviousElementSibling
+// console.log(itemlist.previousSibling);
+// console.log(itemlist.previousElementSibling);
+// itemlist.previousElementSibling.innerHTML = 'Hello World';
+
+//create element
+
+var newdiv = document.createElement('div')
+newdiv.textContent = "new div";
+newdiv.className = "hello1";
+newdiv.id = "hello1"
+newdiv.setAttribute("title","newdiv");
+newdivtext = document.createTextNode("hello world new div here");
+newdiv.appendChild(newdivtext)
+// or newdiv.append(newdivtext);
+console.log(newdiv);
+
+//insert this div
+var container = document.querySelector('header .container');
+var h1 = document.querySelector("header h1")
+container.insertBefore(newdiv,h1);
+
+
+
+
+
 
