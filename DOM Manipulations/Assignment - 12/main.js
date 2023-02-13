0dfe13d6f3044f3eccdeb69f42ec98eb476a0dc1
Vishal-Merugu <vishalvish360@gmail.com>
2023-02-13 13:08:07 +0530

    Assignment - 14

--- DOM Manipulations/Assignment - 12/main.js
+++ DOM Manipulations/Assignment - 12/main.js
@@ -14,21 +14,42 @@ document.addEventListener('DOMContentLoaded',()=>{
         localStorage.setItem(username,JSON.stringify(details));
         var li = document.createElement('li')
         var del = document.createElement('button')
+        var edit = document.createElement('button')
+
         del.textContent = "delete";
         del.className = "delete";
-        li.textContent = `username = ${username} , email = ${email} and phone = ${phone}`
-        li.id = username
-        li.append(del)
+
+        edit.textContent = "Edit"
+        edit.className = "edit"
+
+        li.textContent = `username = ${username} , email = ${email} and phone = ${phone}`;
+        li.id = username;
+        li.append(del);
+        li.append(edit)
         console.log(li);
-        document.querySelector('#details').append(li)
+        document.querySelector('#details').append(li);
+
+        document.querySelector('#username').value = '';
+        document.querySelector('#email').value = '';
+        document.querySelector('#phone').value = '';
     }
 
     document.querySelector('#details').onclick = (e)=>{
-        if(e.target.className === "delete"){
+        if(e.target.className === "delete" || e.target.className === 'edit'){
+
+
             var id = e.target.parentNode.id;
+            var temp_details  = JSON.parse(localStorage.getItem(id))
             localStorage.removeItem(id)
-            document.getElementById(id).remove();            
+            document.getElementById(id).remove();   
+            console.log(temp_details);
+
+            if(e.target.className === 'edit'){
+                document.querySelector('#username').value = temp_details.username;
+                document.querySelector('#email').value = temp_details.email;
+                document.querySelector("#phone").value = temp_details.phone;
+            }           
+    
         }
     }
-
 });
